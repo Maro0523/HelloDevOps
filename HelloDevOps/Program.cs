bddcb01d6cc3bdce28aa19532b2c11ad8e1c5dcc
Maro0523 <martinpan23@gmail.com>
2026-05-25 18:06:30 +0200

    Add project files.

---
+++ HelloDevOps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloDevOps
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello DevOps");
+        }
+    }
+}
---
+++ TestProject2/Test1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System;
+using HelloDevOps;
+
+namespace TestProject2
+{
+    [TestClass]
+    public class Test1
+    {
+        [TestMethod]
+        public void Test_ConsoleOutput_Content()
+        {
+            using var sw = new StringWriter();
+            Console.SetOut(sw);
+
+            Program.Main(Array.Empty<string>());
+
+            var result = sw.ToString().Trim();
+            Assert.AreEqual("Hello DevOps", result);
+        }
+
+        [TestMethod]
+        public void EnnekAzIssueMiattElKellBuknia()
+        {
+            Assert.Fail("Pipeline teszt hiba Issue generáláshoz.");
+        }
+    }
+}
